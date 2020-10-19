ed90160062978a9048a8574d6fb79be3b691e7c4
b_a_x <ily-odinokov@yandex.ru>
2020-10-19 14:23:50 +0300

    Second

---
+++ src/Ini/IniFile.cs
@@ -0,0 +1,97 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Ini
+{
+    /// <summary>
+    /// Класс для работы с ini
+    /// </summary>
+    public class IniFile
+    {
+        private const byte size = 20;
+
+        /// <summary>
+        /// Полный путь к файлу
+        /// </summary>
+        public readonly string Path;
+
+        /// <summary>
+        /// kernel32.dll и его функция WritePrivateProfilesString
+        /// </summary>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        [DllImport("kernel32.dll")]
+        private static extern long WritePrivateProfileString(string section, string key, string value, string filePath);
+
+        /// <summary>
+        /// kernel32.dll и функция GetPrivateProfileString
+        /// </summary>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <param name="default"></param>
+        /// <param name="retVal"></param>
+        /// <param name="size"></param>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        [DllImport("kernel32.dll")]
+        private static extern int GetPrivateProfileString(string section, string key, string @default, StringBuilder retVal, int size, string filePath);
+
+        public IniFile(string iniPath)
+        {
+            Path = new FileInfo(iniPath).FullName;
+        }
+
+        /// <summary>
+        /// Читаем ini-файл и возвращаем значение указного ключа из заданной секции
+        /// </summary>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string ReadIni(string section, string key)
+        {
+            var retVal = new StringBuilder(size);
+            GetPrivateProfileString(section, key, string.Empty, retVal, size, Path);
+            return retVal.ToString();
+        }
+
+        public void Write(string section, string key, string value)
+        {
+            WritePrivateProfileString(section, key, value, Path);
+        }
+
+        /// <summary>
+        /// Удаляем ключ из выбранной секции 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="section"></param>
+        public void DeleteKey(string key, string section = null)
+        {
+            Write(section, key, null);
+        }
+
+        /// <summary>
+        /// Удаляем выбранную секцию
+        /// </summary>
+        /// <param name="section"></param>
+        public void DeleteSection(string section = null)
+        {
+            Write(section, null, null);
+        }
+
+        /// <summary>
+        /// Проверяем, есть ли такой ключ, в этой секции 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="section"></param>
+        /// <returns></returns>
+        public bool KeyExists(string key, string section = null)
+        {
+            return ReadIni(section, key).Length > 0;
+        }
+    }
+}
---
+++ src/Ini/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace Ini
+{
+    public class Program
+    {
+        private const byte s = 1;
+        private const byte k = 3;
+        
+        static void Main(string[] args)
+        {
+            IniFile ini = new IniFile("config.ini");
+
+            ini.Write("Test", "Test", "Test");
+
+            if (File.Exists(ini.Path) == false)
+            {
+                Console.WriteLine("Отсутствует файл config.ini");
+                return;
+            }
+
+            try
+            {
+                if (ini.KeyExists(args[k], args[s]) == false)
+                {
+                    Console.WriteLine("Заданной пары СЕКЦИЯ ПАРАМЕТР нет в конфигурационном файл");
+                    return;
+                }
+
+                Console.WriteLine($"Секция: {args[s]}");
+                Console.WriteLine($"Ключ: {args[k]}");
+                Console.WriteLine($"Значение: {ini.ReadIni(args[s], args[k])}");
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Неправлино переданы параметры");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка приложения");
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
